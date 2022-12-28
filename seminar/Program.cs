a5169bdc23db316d9d61450ae83dab092498645c
Victoria <si4yova2010@yandex.ru>
2022-12-28 21:15:21 +0500

    solution for task1/seminar 3

--- seminar/Program.cs
+++ seminar/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
     while(true)
     {
-        string input = Console.ReadLine();
+        string input = Console.ReadLine()!;
         if(!input.Equals("exit"))
             Console.WriteLine("{0}->{1}",input, input[1]);
         else
---
+++ seminar3_task1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причем Х,Y не равны нулю
+// и выдаёт номер четверти плоскости, в которой находится эта точка
+Console.WriteLine("Hello, World!");
+
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+int getQuarterNumber(int x, int y)
+{
+    int quarter = 0;
+    if (x > 0 && y > 0)  // && - это "и" в С#
+    {
+        quarter = 1;
+    }
+    else if (x < 0 && y > 0)
+    {
+        quarter = 2;
+    }
+    else if (x < 0 && y < 0)
+    {
+        quarter = 3;
+    }
+    else if (x > 0 && y < 0)
+    {
+        quarter = 4;
+    }
+    return quarter;
+}
+
+int userX = getUserValue("Введите Х");
+int userY = getUserValue("Введите Y");
+
+if (userX != 0 && userY != 0)
+{
+    int quarter = getQuarterNumber(userX, userY);
+    Console.WriteLine($"Точка [{userX}, {userY}] находится в {quarter} четверти");
+}
+else
+{
+    Console.WriteLine($"Точка [{userX}, {userY}] находится на оси координат");
+}
