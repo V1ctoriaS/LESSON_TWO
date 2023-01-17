d97ab4ca3b689761291d33febcf6f1087f0e157e
Victoria <si4yova2010@yandex.ru>
2023-01-17 21:06:52 +0500

    seminar 4, task №2

---
+++ seminar4_task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает число и выдает количество цифр в этом числе
+
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int getNumberOfDigit(int number)
+{
+    int NumberOfDigit = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        NumberOfDigit++;
+    }
+    return NumberOfDigit;
+}
+
+int number = getUserData("Введите число");
+int NumberOfDigit = getNumberOfDigit(number);
+Console.WriteLine($"В числе {number} - {NumberOfDigit} цифр");
