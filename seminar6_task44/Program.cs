5d05e1ef606502b59b24717e04eb46ee432d5a1b
Victoria <si4yova2010@yandex.ru>
2023-01-29 22:52:44 +0500

    task 44

---
+++ seminar6_task44/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+//Первые два числа Фибоначчи: 0 и 1.
+//Если N = 5 -> 0 1 1 2 3
+//Если N = 3 -> 0 1 1
+//Если N = 7 -> 0 1 1 2 3 5 8
+
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+string TransformFibonnaci (int number)
+{
+    //string result = string.Empty;
+    int n1 = 0;
+    int n2 = 1;
+    string result = $"{n1}{n2}";    //эта функция преобразует int в string
+    for (int i = 2; i < number; i++)
+    {
+       int n = n1 + n2;
+       result = result + n;
+       n1 = n2;
+       n2 = n;
+    }
+    return result;
+}
+
+int number = getUserData("Введите число");
+string result = TransformFibonnaci(number);
+Console.WriteLine(result);
