06ed8dbbd457e49b07e7b3ffb0b308730dc94093
Victoria <si4yova2010@yandex.ru>
2023-01-29 19:33:19 +0500

    task 39

---
+++ seminar6_task39/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая перевернет одномерный массив
+//[1 2 3 4 5]->[5 4 3 2 1]
+int [] generateArray(int length, int deviation)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(-deviation, deviation + 1);
+    }
+    return array;
+}
+
+void printArray(int[] array)       //эта функция выводит ранее созданный массив на печать
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] );
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int[] reverseArray(int[] array)
+{
+    int fullLength = array.Length;
+    int length = array.Length / 2;
+    for (int i = 0; i < length; i++)
+    {
+        int buf = array[i];
+        array[i] = array[fullLength - i - 1];
+        array[fullLength - i - 1] = buf;
+    }
+    return array;
+}
+
+int[] array = generateArray(11, 20);
+printArray(array);
+int[] reversed = reverseArray(array);
+printArray(reversed);
