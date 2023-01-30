a88de431cdabb3b53f7eedde078498703c1d423c
Victoria <si4yova2010@yandex.ru>
2023-01-30 21:42:08 +0500

    task 45

---
+++ seminar6_task45/Program.cs
@@ -0,0 +1,49 @@
+﻿ // Задача 45: Напишите программу, которая будет создавать копию заданного массива с 
+ //помощью поэлементного копирования.
+
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int[] generateArray(int length, int start, int end)   // эта функция создает массив
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(start, end + 1);    //+1 потому что поумолчанию последнее значение не используется рандомайзером
+    }
+    return result;
+}
+
+int [] imitateArray (int [] array)
+{
+    int[] result = new int [array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[i];
+    }
+    return result;
+}
+
+void printArray(int[] array)       //эта функция выводит ранее созданный массив на печать
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] );
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int length = getUserData("Введите длину массива");
+int[] array = generateArray(length, -10,10);
+printArray(array);
+int [] result = imitateArray(array);
+printArray(result);
