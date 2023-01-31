71a9f3dc3302b1def9e28bd48630874b1a37a508
Victoria <si4yova2010@yandex.ru>
2023-01-31 16:26:11 +0500

    task 41 finalize late

---
+++ seminar6_task41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+int mes = getUserData("Введите желаемое количество чисел");
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+string SetOfNumbers (int number)
+{
+    string arr = string.Empty;
+    int result = 0;
+    while (number => 8)
+    {
+        if (number > 0)
+        {
+            result++;
+            arr = $"{result}";
+        }
+        else
+        {
+            result = result + 0;
+            arr = $"{result}";
+        }
+    }
+    return result;
+}
+
+string numbers = SetOfNumbers(number);
+Console.WriteLine($"Число {numbers}");
