910c07b80bce181fbe68a68cad5eade990c44144
Victoria <si4yova2010@yandex.ru>
2023-02-05 18:53:34 +0500

    task 50

---
+++ seminar7_task50/Program.cs
@@ -0,0 +1,23 @@
+﻿
+        void Main(string[] args)
+        {
+            int[,] number = new int[3, 5];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    Console.WriteLine("Please enter number");
+                    number[x, i] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    Console.Write(number[x, i] + " ");
+                }
+                Console.WriteLine(" ");
+                Console.ReadLine();
+            }
+        }
